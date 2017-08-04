5907f68fb9b1617deb2d09e003d5edf943222aee
ALfredo <30509479+xtraineralfredo@users.noreply.github.com>
2017-08-04 15:22:16 -0500

    Update media.py
    Fixed Bugs

--- movies/media.py
+++ movies/media.py
@@ -7,20 +7,21 @@ class Movie():
         poster image, and a movie trailer for the user to watch when they
         click on the image of the poster."""
 
+        def __init__(self, movie_title, movie_storyline, poster_image,
+                     trailer_youtube):
+                """The inputs here contain four attributes referencing to
+                the movie object including the title, storyline, poster, and
+                trailer. The __init__ function allows for multiple movie
+                objects to be created."""
+                self.title = movie_title
+                self.storyline = movie_storyline
+                self.poster_image_url = poster_image
+                self.trailer_youtube_url = trailer_youtube
 
-def __init__(self, movie_title, movie_storyline, poster_image,
-             trailer_youtube):
-        """The inputs here contain four attributes referencing to
-        the movie object including the title, storyline, poster, and
-        trailer. The __init__ function allows for multiple movie objects
-        to be created."""
-self.title = movie_title
-self.storyline = movie_storyline
-self.poster_image_url = poster_image
-self.trailer_youtube_url = trailer_youtube
-# This function calls for a method to access the movie object and
-# be opened through the users browser.
-
-
-def show_trailer(self):
-    webbrowser.open(self.trailer_youtube_url)
+        # This function calls for a method to access the movie object and
+        # be opened through the users browser.
+        def show_trailer(self):
+            """
+            Intitializing instance for opening the youtube video
+            """
+            webbrowser.open(self.trailer_youtube_url)
